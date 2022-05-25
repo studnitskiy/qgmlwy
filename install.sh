e36f08a21b9c51e3bef9e38fcc51093ea143eb9f
Alexander Studnitskiy <astudnitskiy@gmail.com>
2022-05-24 20:01:05 -0700

    Patch based on Ubuntu 22.04

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-cp ./usr/share/X11/xkb/rules/* /usr/share/X11/xkb/rules
+patch /usr/share/X11/xkb/rules/base.lst ./usr/share/X11/xkb/rules/base.lst.patch
+patch /usr/share/X11/xkb/rules/base.xml ./usr/share/X11/xkb/rules/base.xml.patch
+cp /usr/share/X11/xkb/rules/base.lst /usr/share/X11/xkb/rules/evdev.lst
+cp /usr/share/X11/xkb/rules/base.xml /usr/share/X11/xkb/rules/evdev.xml
 cp ./usr/share/X11/xkb/symbols/* /usr/share/X11/xkb/symbols
